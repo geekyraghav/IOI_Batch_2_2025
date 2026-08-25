0f669be291ddf8dcd04679c627b5ed5fa688bb18
geekyraghav <49072935+geekyraghav@users.noreply.github.com>
2026-08-25 09:07:46 +0530

    Create NextSmallerElement.cpp

---
+++ Stacks/NextSmallerElement.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> nextSmallerEle(vector<int>& arr) {
+        stack<int> st;
+        int n = arr.size();
+        vector<int> nge(n);
+        for(int i=n-1;i>=0;i--){
+            while(st.size() > 0 && st.top() >= arr[i]) 
+                st.pop();
+            if(st.size()==0) nge[i] = -1;
+            else nge[i] = st.top();
+            st.push(arr[i]);
+        }
+        return nge;
+    }
+};
