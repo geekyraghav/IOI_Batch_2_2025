b76661d2277c5342c1b0a2662495d5122cd9b719
geekyraghav <49072935+geekyraghav@users.noreply.github.com>
2026-09-03 15:29:22 +0530

    Create FirstNegetiveInWindow.cpp

---
+++ Queues/FirstNegetiveInWindow.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> firstNegInt(vector<int>& arr, int k) {
+        int n = arr.size();
+        vector<int> ans(n-k+1,0);
+        queue<pair<int,int>> q;
+        for(int i=0;i<n;i++){
+            if(arr[i] < 0) q.push({arr[i],i});
+        }
+        for(int i=0;i<n-k+1;i++){ // ith window start
+            while(q.size() > 0 && q.front().second < i) q.pop();
+            if(q.size() > 0 && q.front().second < i+k) ans[i] = q.front().first;
+        }
+        return ans;
+    }
+};
