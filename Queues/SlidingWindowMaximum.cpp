4168af338387d62114fdcdd6325766d789f7d3b8
geekyraghav <49072935+geekyraghav@users.noreply.github.com>
2026-09-15 23:56:25 +0530

    Create SlidingWindowMaximum.cpp

---
+++ Queues/SlidingWindowMaximum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        if(k==1) return nums;
+        int n = nums.size();
+        deque<int> dq; // decreasing order of ele (index)
+        vector<int> ans; // i should be min k-1
+        for(int i=0;i<n;i++){
+            while(dq.size()>0 && nums[i]>nums[dq.back()]) 
+            dq.pop_back();
+            dq.push_back(i);
+            int j = i-k+1; // start of the window
+            while(dq.front()<j) dq.pop_front();
+            if(i>=k-1) ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
+};
