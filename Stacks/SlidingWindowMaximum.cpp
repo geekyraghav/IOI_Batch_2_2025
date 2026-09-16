f5a4ad6a70cd113416387d1b928a4743eceafd38
geekyraghav <49072935+geekyraghav@users.noreply.github.com>
2026-09-16 13:37:06 +0530

    Update SlidingWindowMaximum.cpp

--- Stacks/SlidingWindowMaximum.cpp
+++ Stacks/SlidingWindowMaximum.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-        if(k==1) return arr;
         int n = arr.size();
-        int ngi[n];
         stack<int> st;
-        ngi[n-1] = n;
-        st.push(n-1);
-        for(int i=n-2;i>=0;i--){
-            while(st.size()>0 && arr[st.top()]<=arr[i]){
+        vector<int> nge(n);
+        for(int i=n-1;i>=0;i--){
+            while(st.size() > 0 && arr[st.top()] <= arr[i])
                 st.pop();
-            }
-            if(st.size()==0) ngi[i] = n;
-            else ngi[i] = st.top();
+            if(st.size() != 0) nge[i] = st.top();
+            else nge[i] = -1;
             st.push(i);
         }
-        vector<int> ans;
+        vector<int> ans(n-k+1);
         int j = 0;
-        for(int i=0;i<n-k+1;i++){
-            if(j<i) j = i;
-            int mx = arr[j]; // starting of window se
-            while(j < i+k){ // means if nge is inside window
-                mx = arr[j];
-                if(ngi[j] >= i+k) break;
-                j = ngi[j];
-            }
-            ans.push_back(mx);
+        for(int i=0;i<n-k+1;i++){ // traversing all windows
+            if(j < i) j = i;
+            while(nge[j] != -1 && nge[j] < i+k) j = nge[j];
+            ans[i] = arr[j];
         }
         return ans;
     }
